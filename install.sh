d4155fbbaa079170f40e3a067022d709ef695dfa
Michał Pomarański <michal.pomaranski@gmail.com>
2017-03-25 09:55:10 +0100

    renamed jobs done

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ ln -s ~/.dotfiles/.vim ~/.vim
 # EMACS
 ln -s ~/.dotfiles/.spacemacs ~/.spacemacs
 ln -s ~/.dotfiles/.emacs.d ~/.emacs.d
+
+# FISH
+ln -s ~/.dotfiles/.config/fish ~/.config/fish
+ln -s ~/.dotfiles/jobsdone.mp3 ~/jobsdone.mp3
